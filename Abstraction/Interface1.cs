171fc52e36a3da5d3b77e4dd69b1c86d1f570f42
Uma Mahesh Kadiyala <UKadiyala@ad.prolifics.com>
2019-12-20 09:20:03 +0000

    Add project files.

---
+++ Abstraction/Interface1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstraction
+{
+    interface Interface1
+    {
+        abstract public void Facemethod();
+
+    }
+}
